113763a31ab31eff3f518febb8a4b42b9c31e6f6
Rüştü AKKAN <rustu-akkan@hotmail.com>
2026-09-20 10:14:50 +0300

    Blueprint eklendi

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from config import Yapilandirma
 from app.database import veri_tabanini_baslat
+from chatbot_routes import chatbot_bp
+
 app = Flask(__name__)
 app.config.from_object(Yapilandirma)
 CORS(app, resources={r"/*": {"origins": Yapilandirma.IZIN_VERILEN_KAYNAKLAR}})
 veri_tabanini_baslat(app)
+
+app.register_blueprint(chatbot_bp)
+
 from app import routes
